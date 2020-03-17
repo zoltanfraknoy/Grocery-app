bfdbc84d2d7699408bdbfdec760bf38e1f63ec88
Zoltán <zoltanfraknoy@gmail.com>
2020-03-17 19:44:15 +0100

    Grocery-app started

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './components/main/main.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'main' },
+  { path: 'main', component: MainComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
